99bb860f0577c54c2b2cac6ca9762440167c6b5e
ZiadSala7 <zslah2800@gmail.com>
2024-11-25 12:27:09 +0200

    splash feature

--- lib/core/app/food_delivery_app.dart
+++ lib/core/app/food_delivery_app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/features/splash/presentation/views/splash_view.dart';
 
 class FoodDeliveryApp extends StatelessWidget {
   const FoodDeliveryApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: SplashView(),
+    );
   }
 }
