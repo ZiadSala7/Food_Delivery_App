83b6caacbe23edd26dbc7d3584ea142e71face96
ZiadSala7 <zslah2800@gmail.com>
2024-11-25 12:18:45 +0200

    initializing app

---
+++ lib/core/app/food_delivery_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FoodDeliveryApp extends StatelessWidget {
+  const FoodDeliveryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
