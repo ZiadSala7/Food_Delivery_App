e4056c890c921bb74d4897f6a037f175c05bd490
ZiadSala7 <zslah2800@gmail.com>
2024-11-26 17:42:29 +0200

    create registerView

---
+++ lib/features/auth/register/presentation/views/register_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/features/auth/common/custom_common_auth_background.dart';
+import 'package:food_delivery_app/features/auth/register/presentation/views/widgets/register_view_body.dart';
+
+class RegisterView extends StatelessWidget {
+  const RegisterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xfff5f6fe),
+      body: CustomCommonAuthBackground(widget: RegisterViewBody()),
+    );
+  }
+}
