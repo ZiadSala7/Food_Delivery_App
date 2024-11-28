8ffb9b87a1f90f755e48269d9ed10b93eafe61ca
ZiadSala7 <zslah2800@gmail.com>
2024-11-28 14:20:18 +0200

    confirmOtp Refactors

--- lib/features/auth/forget_password/presentation/views/confirm_otp_view.dart
+++ lib/features/auth/forget_password/presentation/views/confirm_otp_view.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:food_delivery_app/constants.dart';
-import 'package:food_delivery_app/core/common/widgets/custom_button.dart';
-import 'package:food_delivery_app/core/utils/app_text_styles.dart';
-import 'package:food_delivery_app/core/utils/my_strings.dart';
 import 'package:food_delivery_app/features/auth/common/custom_common_auth_background.dart';
-import 'package:food_delivery_app/features/auth/forget_password/presentation/views/widgets/custom_arrow_back_button.dart';
-import 'package:pinput/pinput.dart';
+import 'package:food_delivery_app/features/auth/forget_password/presentation/views/widgets/confirm_otp_view_body.dart';
 
 class ConfirmOtpView extends StatelessWidget {
   const ConfirmOtpView({super.key});
@@ -14,49 +9,6 @@ class ConfirmOtpView extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: CustomCommonBackground(widget: ConfirmOtpViewBody()),
-    );
-  }
-}
-
-class ConfirmOtpViewBody extends StatelessWidget {
-  const ConfirmOtpViewBody({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const SizedBox(height: 50),
-        CustomArrowBackButton(onPressed: Navigator.of(context).pop),
-        const SizedBox(height: 20),
-        const Text(
-          MyStrings.codeDigits,
-          style: AppTextStyles.text30Bold,
-          textAlign: TextAlign.left,
-        ),
-        const SizedBox(height: 20),
-        const Text(
-          MyStrings.codeDesc,
-          style: AppTextStyles.text16Reg,
-        ),
-        const SizedBox(height: 70),
-        Container(
-          decoration: const BoxDecoration(
-            color: Color.fromARGB(182, 245, 245, 245),
-          ),
-          width: double.infinity,
-          child: Pinput(
-            onCompleted: (value) {},
-            defaultPinTheme: defaultPinTheme,
-            focusedPinTheme: focusedPinTheme,
-          ),
-        ),
-        const SizedBox(height: 270),
-        CustomButton(
-          onPressed: () {},
-          txt: MyStrings.next,
-        ),
-      ],
     );
   }
 }
---
+++ lib/features/auth/forget_password/presentation/views/widgets/confirm_otp_view_body.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/constants.dart';
+import 'package:food_delivery_app/core/common/widgets/custom_button.dart';
+import 'package:food_delivery_app/core/utils/app_text_styles.dart';
+import 'package:food_delivery_app/core/utils/my_strings.dart';
+import 'package:food_delivery_app/features/auth/forget_password/presentation/views/widgets/custom_arrow_back_button.dart';
+import 'package:pinput/pinput.dart';
+
+class ConfirmOtpViewBody extends StatelessWidget {
+  const ConfirmOtpViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 50),
+        CustomArrowBackButton(onPressed: Navigator.of(context).pop),
+        const SizedBox(height: 20),
+        const Text(
+          MyStrings.codeDigits,
+          style: AppTextStyles.text30Bold,
+          textAlign: TextAlign.left,
+        ),
+        const SizedBox(height: 20),
+        const Text(
+          MyStrings.codeDesc,
+          style: AppTextStyles.text16Reg,
+        ),
+        const SizedBox(height: 70),
+        Container(
+          decoration: const BoxDecoration(
+            color: Color.fromARGB(182, 245, 245, 245),
+          ),
+          width: double.infinity,
+          child: Pinput(
+            onCompleted: (value) {},
+            defaultPinTheme: defaultPinTheme,
+            focusedPinTheme: focusedPinTheme,
+          ),
+        ),
+        const SizedBox(height: 270),
+        CustomButton(
+          onPressed: () {},
+          txt: MyStrings.next,
+        ),
+      ],
+    );
+  }
+}
