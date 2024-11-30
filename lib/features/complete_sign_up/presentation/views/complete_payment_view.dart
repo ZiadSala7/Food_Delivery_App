d8e9b49ba7728944247424cdffefd3a8118f22c8
ZiadSala7 <zslah2800@gmail.com>
2024-11-30 13:49:28 +0200

    completePaymentView has been completed

--- lib/features/complete_sign_up/presentation/views/complete_payment_view.dart
+++ lib/features/complete_sign_up/presentation/views/complete_payment_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/core/common/widgets/custom_button.dart';
 import 'package:food_delivery_app/core/common/widgets/custom_common_background.dart';
 import 'package:food_delivery_app/core/common/widgets/custom_common_texts_widget.dart';
+import 'package:food_delivery_app/core/utils/app_assets.dart';
 import 'package:food_delivery_app/core/utils/my_strings.dart';
 
 class CompletePaymentView extends StatelessWidget {
@@ -19,14 +21,60 @@ class CompletePaymentViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        CustomCommonTextsWidget(
+        const CustomCommonTextsWidget(
           headerTxt: MyStrings.paymentMethod,
           descriptionTxt: MyStrings.commonDesc,
         ),
+        const SizedBox(height: 20),
+        CustomPaymentMethodButton(
+          image: Assets.iconsPaypalIcon,
+          onTap: () {},
+        ),
+        const SizedBox(height: 20),
+        CustomPaymentMethodButton(
+          image: Assets.iconsVisaIcon,
+          onTap: () {},
+        ),
+        const SizedBox(height: 20),
+        CustomPaymentMethodButton(
+          image: Assets.iconsPayoneerIcon,
+          onTap: () {},
+        ),
+        const SizedBox(height: 147),
+        CustomButton(onPressed: () {}, txt: MyStrings.next),
       ],
+    );
+  }
+}
+
+class CustomPaymentMethodButton extends StatelessWidget {
+  final String image;
+  final Function() onTap;
+  const CustomPaymentMethodButton({
+    super.key,
+    required this.image,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Card(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Center(
+            child: Image.asset(
+              image,
+              height: 50,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
